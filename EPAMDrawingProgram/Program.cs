842235ace371439522530188113b1eabaf6409c7
jludden <jasonludden@gmail.com>
2018-06-02 10:38:49 +0800

    Add project files.

---
+++ EPAMDrawingProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPAMDrawingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
