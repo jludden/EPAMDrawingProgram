4acfbe775b30082f013a1392b9132ff646a99bcf
jludden <jasonludden@gmail.com>
2018-06-02 10:42:42 +0800

    Creating repo

---
+++ DrawingProgramTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DrawingProgramTests
+{
+    [TestClass]
+    public class DrawingProgramTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
